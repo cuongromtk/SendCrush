81f39804c640aedf2fe97af3d75f4c070ae4844a
cuongromtk <cuongdang9101996@gmail.com>
2024-03-27 19:34:16 +0700

    init project

---
+++ config.js
@@ -0,0 +1,13 @@
+const CONFIG = {
+    title: 'Làm bạn gái anh nha 😙',
+    desc: 'Phải chăng em đã yêu and ngay từ cái nhìn đầu tiên =))',
+    btnYes: 'Em nguyện ý làm bàn gái của anh <33',
+    btnNo: 'Không, Anh trai à :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messDesc: 'Hẹn em tối mai 7h anh qua đón nhé công chúa.',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://www.facebook.com/CuDepTr/' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
